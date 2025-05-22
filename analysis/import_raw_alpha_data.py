c7b36500b19a8b642f02a2ef6d4e389d8e8b96b8
Malte Rudo Güth <maltegueth@gmail.com>
2025-05-22 11:11:03 -0500

    Update import_raw_alpha_data.py

--- analysis/import_raw_alpha_data.py
+++ analysis/import_raw_alpha_data.py
@@ -155,3 +155,56 @@ for sub in subs:
 
     # Save processed data frame
     data.to_csv('output_path/' + sub + '_performance_' + task + '_fpga.csv', index=False)
+
+#########
+
+# Process eventIDE data
+path = 'path to raw data'
+# Because of the output format of eventIDE two paths are needed
+# and phase targets were run in separate files
+path2 = '/Prediction/SignalFileWriter/SignalData/SignalData_markers.csv'
+
+eventideStatsDF = pd.DataFrame(np.zeros(shape=(0, 8)))
+eventideStatsDF.columns = ['target', 'phase', 'phase_error', 'envelope', 'frequency', 'eyes', 'sensitivity', 'subject']
+eventideStatsDF = eventideStatsDF.astype({'target': str, 'phase': float, 'phase_error': float, 'envelope': float,
+                                  'frequency': float, 'eyes': str, 'sensitivity': float, 'subject': int})
+
+data = pd.DataFrame(np.zeros(shape=(0, 6)), columns=columns)
+data = data.astype({'samples': float, 'signal': float, 'stimMarker': str,
+                    'stimTime': float, 'stim': int, 'diesdas': int})
+data = data.drop(columns=['stimMarker', 'diesdas'])
+
+for sub in subs:
+
+    for target in targets:
+        # Concatenate the path and the file
+        file = path + 'sub' + sub + '_' + task + '_' + target + path2
+        # Read the file into a pandas data frame
+        current_data = pd.read_csv(file, sep=',', names=columns, skiprows=1, header=None)
+        current_data = current_data.fillna(0)
+        current_data = current_data.drop(columns=['stimMarker', 'diesdas'])
+        current_data['target'] = target
+        data = pd.concat([data, current_data], axis=0)
+
+    # Set the order of phase targets
+    targetDict = info[(info['subject'] == int(sub)) & (info['task'] == task)].iloc[:,2:6].to_numpy()
+
+    # Cut out the calibration and instructions portion
+    # and skip the first 5 seconds of every new block (accounting for switching delay)
+    cropped_data = pd.DataFrame(np.zeros(shape=(0, data.shape[1])))
+    cropped_data.columns = data.columns
+    cropped_data = cropped_data.astype(data.dtypes.to_dict())
+
+    for target in targets:
+        current_data = data[data['target'] == target]
+        current_data.reset_index(drop=True, inplace=True)
+        current_data.drop(index=current_data.index[:5000], axis=0, inplace=True)
+        cropped_data = pd.concat([cropped_data, current_data])
+
+    data = cropped_data
+    # The code for stim stays in the digital channel for 2 ms, remove the second instance of 1
+    data['stim'] = data['stim'].where((data['stim'] != 1) | (data['stim'].shift() != 1), 0)
+    data.reset_index(drop=True, inplace=True)
+  
+    # Save processed data frame
+    data.to_csv('output_path/' + sub + '_performance_' + task + '_eventide.csv', index=False)
